08a6d2d769ddb5cf8d54184272c850d41bfe8f30
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-07 19:31:44 -0500

    add put and delete handlers

--- routes/accounts.js
+++ routes/accounts.js
@@ -4,12 +4,28 @@ const db = require('../data/dbConfig');
 //
 //Get
 router.get('/', async (req, res) => {
-    let accounts = await db('accounts');
+    let accounts = await db.select().table('accounts');
     if (!accounts || accounts == null) {
         res.status(500).json({ error: 'There was an error' });
     }
     console.log(accounts);
     res.json(accounts);
+});
+
+//
+//Get specific account
+router.get('/:id', async (req, res) => {
+    console.log(req.params);
+    try {
+        let account = await db('accounts').where('id', req.params.id);
+        console.log(account);
+        if (!account || account == null) {
+            res.status(500).json({ error: 'Failed to get account' });
+        }
+        res.status(200).json(account);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to get account' });
+    }
 });
 
 //
@@ -23,6 +39,33 @@ router.post('/', async (req, res) => {
         message: 'Successfully saved account',
         account: inserted,
     });
+});
+
+//
+//Put
+router.put('/:id', async (req, res) => {
+    try {
+        let updated = await db('accounts')
+            .where('id', req.params.id)
+            .update({ name: req.body.name, budget: req.body.budget });
+        res.status(200).json({ message: 'success', updated });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//
+//Delete
+router.delete('/:id', async (req, res) => {
+    try {
+        let deleted = await db('accounts')
+            .where('id', req.params.id)
+            .del();
+        if (!deleted || deleted == null) {
+            res.status(500).json({ error: 'error deleting account' });
+        }
+        res.status(200).json({ message: 'successfully deleted', deleted });
+    } catch (err) {}
 });
 
 module.exports = router;
